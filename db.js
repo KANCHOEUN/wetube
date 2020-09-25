a88a43c0ca0e6c6966a8b3b59dce99a934c5c558
Hia <kancho1216@naver.com>
2020-09-26 06:09:49 +0900

    view, control, dummy data

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 12345,
+        title: "Awesome Video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 54321,
+            name: "Hyeeun",
+            email: "cho@eun.com"
+        }
+    },
+    {
+        id: 123456,
+        title: "Nice Video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 654321,
+            name: "Hia",
+            email: "hia@eun.com"
+        }
+    },
+    {
+        id: 1234,
+        title: "First Video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 4321,
+            name: "Choeun",
+            email: "cho@cho.com"
+        }
+    },
+    {
+        id: 1234567,
+        title: "Hello Video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 7654321,
+            name: "Kancho",
+            email: "kan@cho.com"
+        }
+    }
+]
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import routes from "../routes";
-import { videos, upload, editVideo, deleteVideo, videoDetail } from "../controllers/videoController";
+import { getUpload, postUpload, editVideo, deleteVideo, videoDetail } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail(), videoDetail);
 
 export default videoRouter;
