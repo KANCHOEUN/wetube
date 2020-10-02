99b13045638aa82b89e775b68ae4a011d037da62
Hia <kancho1216@naver.com>
2020-10-03 02:16:50 +0900

    video upload, edit, delete and video detail

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,16 @@
 import express from "express";
 import routes from "../routes";
-import { getUpload, postUpload, editVideo, deleteVideo, videoDetail } from "../controllers/videoController";
+import { getUpload, postUpload, getEditVideo, deleteVideo, videoDetail, postEditVideo } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
 videoRouter.get(routes.videoDetail(), videoDetail);
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
